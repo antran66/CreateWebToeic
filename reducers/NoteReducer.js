e42263d55a3533cde184c727178a963250089b60
Paul An <49549655+antran66@users.noreply.github.com>
2019-10-13 22:03:08 +0700

    Add files via upload
    change

---
+++ reducers/NoteReducer.js
@@ -0,0 +1,15 @@
+
+
+export default function (state={}, action){
+    switch(action.type){
+        case "NOTES_FETCH":
+            
+            return {
+                ...state, 
+                notesList:action.payload
+            }
+
+        default:
+            return state
+    }
+}
